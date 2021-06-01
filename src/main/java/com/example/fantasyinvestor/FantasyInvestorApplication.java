f0331ea6b077fea0aacc29905e492d8f03ba3e42
Bogu <adambozek1@gmail.com>
2021-06-01 20:30:52 +0200

    Initial commit

---
+++ src/main/java/com/example/fantasyinvestor/FantasyInvestorApplication.java
@@ -0,0 +1,13 @@
+package com.example.fantasyinvestor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FantasyInvestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FantasyInvestorApplication.class, args);
+    }
+
+}
